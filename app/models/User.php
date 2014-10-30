8f0b3917749fd8d4938c2a20ee61cb883f51489d
Tim Groeneveld <tim@timg.ws>
2014-10-30 17:18:37 +1100

    Correct the user authentication model so that it works with confide

--- app/models/User.php
+++ app/models/User.php
@@ -1,13 +1,11 @@
 <?php
 
-use Illuminate\Auth\UserTrait;
-use Illuminate\Auth\UserInterface;
-use Illuminate\Auth\Reminders\RemindableTrait;
-use Illuminate\Auth\Reminders\RemindableInterface;
+use Zizaco\Confide\ConfideUser;
+use Zizaco\Confide\ConfideUserInterface;
 
-class User extends Eloquent implements UserInterface, RemindableInterface {
-
-	use UserTrait, RemindableTrait;
+class User extends Eloquent implements ConfideUserInterface
+{
+	use ConfideUser;
 
 	/**
 	 * The database table used by the model.
